bca07dbd25805411cb651d870bc88c083b3a351a
ev_orishko <orishko1990@gmail.com>
2016-10-31 15:22:53 +0200

    LendingPage in production 2

--- frontend/js/pageFirst.js
+++ frontend/js/pageFirst.js
@@ -1,23 +1,12 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     menu: '#menu',
-    lockAnchors: false,
-    anchors: (globalData.userIdentity == 'True') ? ['secondPage', 'thirdPage', 'fourthPage', 'lastPage'] : ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
     navigation: true,
-    navigationTooltips: (globalData.userIdentity == 'True') ? ['Main','Contacts','More','Pricing'] : ['Start','Main','Contacts','More','Pricing'],
-    showActiveTooltip: false,
-    slidesNavigation: true,
-
-    scrollingSpeed: 600,
-    loopBottom: true,
-    loopTop: true,
-
+    navigationTooltips: ['Start','Markets','Advantages','Pricing','Contacts'],
+    slidesNavigation: false,
   });
-
 });
-
-
-
 
 
 
