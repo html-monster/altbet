7c00481d00f127d948be5e6c653dc3cb01107ae7
vlasakh <vladimir.saxarov@yandex.ua>
2016-11-30 14:05:33 +0200

    Переход на Highstock. Раширяем webpack

---
+++ webpackinc/webpack.config.redux.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+const DEST_SERVER = '/../altbetNew/RefactoredCore/Alt.Bet';
+// const DEST_SERVER = '/../../altbetNew/Alt.Bet';
+
+
+var devFlagPlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
+
+module.exports = {
+    devtool: 'cheap-inline-module-source-map',
+
+    entry: "./frontend/js/react/pageMyPosMount.js",
+    output: {
+        path: __dirname + DEST_SERVER + '/Scripts',
+        publicPath: "Scripts/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel",
+                exclude: [/node_modules/, /public/],
+                query: {
+                  plugins: ['transform-runtime'],
+                  presets: ['es2015', 'stage-0', 'react'],
+                },
+            },
+            {
+                test: /\.jsx$/,
+                loader: "babel-loader",
+                exclude: [/node_modules/, /public/],
+                query: {
+                  // plugins: ['transform-runtime'],
+                  presets: ['es2015', 'stage-0', 'react'],
+                },
+            }
+        ]
+    },
+    watch: true,
+
+    watchOptions: {
+        aggregateTimeout: 300
+    },
+
+    plugins: [
+        new WebpackNotifierPlugin({title: 'bundle.js', alwaysNotify: true}),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        // new webpack.HotModuleReplacementPlugin(),
+        // new webpack.NoErrorsPlugin(),
+        devFlagPlugin
+    ],
+};
