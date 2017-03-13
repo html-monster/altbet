faa47120252b235a66a9011c265ba284cb5fda3f
altbet <gadget.letnik@gmail.com>
2017-03-13 12:15:17 +0200

    перевел settings на redux переделал валидацию на user page в settings на redux поправил глобальную стилизацию ошибок полей(теперь новые классы validJs, invalidJs)

--- frontend/js/nonReact/ajax/ajaxUserDataUpdate.js
+++ frontend/js/nonReact/ajax/ajaxUserDataUpdate.js
@@ -1,23 +1,23 @@
-class ajaxUserDataUpdate{
-	static OnSuccessJs(e){
-		let message = $('.wrapper_user_page .setting_form .answer_message');
-		if(e){
-			message.removeClass('valid').addClass('validation-summary-errors').text(e);
-			$('.wrapper_user_page #user_curr_pass').removeClass('valid').addClass('input-validation-error')
-		}
-		else{
-			message.removeClass('validation-summary-errors').addClass('valid')
-						 .text('Your data was successfully changed');
-		}
-
-		setTimeout(() => {
-			message.removeClass('valid validation-summary-errors').text('');
-		}, 5000);
-	}
-
-	static OnFailureJs(x, y){
-		console.log('XMLHTTPRequest object: ', x);
-		console.log('textStatus: ',  y);
-		defaultMethods.showError('The connection to the server has been lost. Please check your internet connection or try again.');
-	}
-}
+// class ajaxUserDataUpdate{
+// 	static OnSuccessJs(e){
+// 		let message = $('.wrapper_user_page .setting_form .answer_message');
+// 		if(e){
+// 			message.removeClass('validJs').addClass('invalidJs').text(e);
+// 			$('.wrapper_user_page #user_curr_pass').removeClass('validJs').addClass('invalidJs')
+// 		}
+// 		else{
+// 			message.removeClass('invalidJs').addClass('validJs')
+// 						 .text('Your data was successfully changed');
+// 		}
+//
+// 		setTimeout(() => {
+// 			message.removeClass('validJs invalidJs').text('');
+// 		}, 5000);
+// 	}
+//
+// 	static OnFailureJs(x, y){
+// 		console.log('XMLHTTPRequest object: ', x);
+// 		console.log('textStatus: ',  y);
+// 		defaultMethods.showError('The connection to the server has been lost. Please check your internet connection or try again.');
+// 	}
+// }
