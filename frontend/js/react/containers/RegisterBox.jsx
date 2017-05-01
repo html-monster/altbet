d5525d280abb7d6df0dec97aabe1ec948d58a7c5
Vladimir <Zotaper@yandex.ru>
2017-05-01 13:13:38 +0300

    Ver 3

--- frontend/js/react/containers/RegisterBox.jsx
+++ frontend/js/react/containers/RegisterBox.jsx
@@ -26,42 +26,46 @@ class RegisterBox extends BaseController
 
     render()
     {
-        return <div className="pop_up_container">
-            <div className="sign_up_content pop_up_content">
-                <div className="header">
-                    <div className="logo_container">
-                        <a href="/" className="logo"><strong>alt.bet</strong></a>
-                    </div>
-                    <div className="warning_container">
-                        <span>Over 18 only</span>
-                    </div>
-                </div>
-                <div className="top_reg">
-                    <div className="wrapper_reg">
-                        <div className="tabs">
-                            {/*<span className="tab">real</span>*/}
-                            {/*<!--<span className="tab">Demo</span>-->*/}
-                            {/*<span className="tab market-make">Market maker</span>*/}
+        return (
+            <div className="sign_up_form pop_up">
+                <div className="pop_up_container">
+                    <div className="sign_up_content pop_up_content">
+                        <div className="header">
+                            <div className="logo_container">
+                                <a href="/" className="logo"><strong>alt.bet</strong></a>
+                            </div>
+                            <div className="warning_container">
+                                <span>Over 18 only</span>
+                            </div>
                         </div>
-                        <div className="tab_content">
-                            <div className="tab_item real">
-                                <span className="close"><span></span></span>
+                        <div className="top_reg">
+                            <div className="wrapper_reg">
+                                <div className="tabs">
+                                    {/*<span className="tab">real</span>*/}
+                                    {/*<!--<span className="tab">Demo</span>-->*/}
+                                    {/*<span className="tab market-make">Market maker</span>*/}
+                                </div>
+                                <div className="tab_content">
+                                    <div className="tab_item real">
+                                        <span className="close"><span></span></span>
 
-                                <RegisterForm onSubmit={this.props.actions.actionFormSubmit.bind(null, this)} />
+                                        <RegisterForm onSubmit={this.props.actions.actionFormSubmit.bind(null, this)} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="confirm pop_up">
+                            <div className="pop_up_container">
+                                <span className="pop_up_content">
+                                    A letter of confirmation of registration has been sent to you by email. Follow the link in the email to activate your Account.
+                                    <button className="btn">Ok</button>
+                                </span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="confirm pop_up">
-                    <div className="pop_up_container">
-                        <span className="pop_up_content">
-                            A letter of confirmation of registration has been sent to you by email. Follow the link in the email to activate your Account.
-                            <button className="btn">Ok</button>
-                        </span>
-                    </div>
-                </div>
             </div>
-        </div>;
+        );
     }
 }
 
