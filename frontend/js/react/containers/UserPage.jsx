7abdf31a296538dabfada7e34060dace5eea4d76
altbet <gadget.letnik@gmail.com>
2017-06-20 14:16:03 +0300

    пофиксил помарку в тексте

--- frontend/js/react/containers/UserPage.jsx
+++ frontend/js/react/containers/UserPage.jsx
@@ -38,7 +38,7 @@ class UserPage extends BaseController
                 <li className={"tab " + (this.props.route.tab === "funds" ? "active" : "")}><Link to={`/funds`}>Funds</Link></li>
                 <li className={"tab " + (this.props.route.tab === "pref" ? "active" : "")}><Link to={`/preferences`}>My Preferences</Link></li>
                 <li className={"tab " + (this.props.route.tab === "sett" ? "active" : "")}><Link to={`/settings`}>Settings</Link></li>
-                <li className={"tab " + (this.props.route.tab === "pass" ? "active" : "")}><Link to={`/change_password`}>ChangePassword</Link></li>
+                <li className={"tab " + (this.props.route.tab === "pass" ? "active" : "")}><Link to={`/change_password`}>Change Password</Link></li>
             </ul>
             <div className="tab_content">
                 <Funds data={{header: $tabHeaderHtml, active: this.props.route.tab === "funds", tab: this.props.params.tabname}}/>
