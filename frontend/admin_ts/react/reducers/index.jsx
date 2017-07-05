17cbce47b7c5b1addbe5e26f64a6924b3b68b145
Vladimir <Zotaper@yandex.ru>
2017-07-05 13:45:30 +0300

    Auto stash before merge of "master" and "origin/Jeka_dev"

--- frontend/admin_ts/react/reducers/index.jsx
+++ frontend/admin_ts/react/reducers/index.jsx
@@ -5,30 +5,37 @@ import FeedEventsReducer from './FeedEventsReducer.ts';
 
 
 let reducers;
+let page = "";
 
 const common = {
 };
 
-let page = "";
 
-if ( globalData.ac.controller === "feed" && globalData.ac.action === "newfeedexchange" ) page = "applyfeedexchange";
-if ( globalData.ac.controller === "feed" && globalData.ac.action === "index" ) page = "xmlfeedevents";
-
-switch (page)
+if( globalData.ac )
 {
-	case "applyfeedexchange":
-		reducers = {
-			newFeedExchange: newFeedExchange,
-			...common,
-		};
-		break;
+	const { controller, action } = globalData.ac;
 
-	case "xmlfeedevents":
-		reducers = {
-			FeedEvents: FeedEventsReducer,
-			...common,
-		};
-		break;
-}
+	if ( controller === "feed" && action === "newfeedexchange" ) page = "applyfeedexchange";
+	if ( controller === "feed" && action === "index" ) page = "xmlfeedevents";
+
+
+	switch (page)
+	{
+		case "applyfeedexchange":
+			reducers = {
+				newFeedExchange: newFeedExchange,
+				...common,
+			};
+			break;
+
+		case "xmlfeedevents":
+			reducers = {
+				FeedEvents: FeedEventsReducer,
+				...common,
+			};
+			break;
+	}
+} // endif
+
 
 export default reducers ? combineReducers(reducers) : null;
