53a23c146a51ab357ce06ed0fbd038a86a0a1e51
Vladimir <Zotaper@yandex.ru>
2017-07-14 10:36:54 +0300

    Gidx компоненты

---
+++ frontend/js/react/reducers/GidxVerificationReducer.ts
@@ -0,0 +1,68 @@
+/// <reference path="../../.d/common.d.ts" />
+
+declare let globalData;
+
+import {
+    ON_BALVAN,
+} from '../constants/ActionTypesGidxVerification.js';
+/// TS_IGNORE
+import {Common} from "../common/Common";
+
+
+
+export default class Reducer
+{
+    // public static USING_TEAM = 1;
+
+
+    private initialState = {
+        Players: [],
+        ...globalData.Model,
+    };
+
+
+
+    init()
+    {
+        /*let loadedData = {};
+        this.initialState = {...this.initialState, ...loadedData};*/
+    }
+
+
+    public actionsHandler(state, action)
+    {
+        state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.initialState;
+
+        switch (action.type)
+        {
+		case SETTING_ON_FILE_LOAD:
+			return {...state, files: action.payload};
+
+		case SETTING_CHANGE_PROGRESS_BAR:
+			return {...state, loadProgress: action.payload};
+
+		case SETTING_LOAD_FILE_ERROR:
+			return {...state, loadError: action.payload};
+
+            default:
+                this.init();
+                return state
+        }
+    }
+
+
+
+    /**
+     * Change current event in dropbox
+     */
+/*
+    private setCurrentEvent(inProps, state)
+    {
+        return {...state};
+    }
+*/
+}
+
+//
+// const $Reducer = new Reducer();
+// export default $Reducer.actionsHandler.bind($Reducer);
